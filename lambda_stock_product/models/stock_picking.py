76396513959052ac54073822ea355ca400fb0823
Zeno Nanon <zna@odoo.com>
2021-01-28 17:26:41 -0800

    [MOD] add feature to create merged pdf in stock.picking

--- lambda_stock_product/models/stock_picking.py
+++ lambda_stock_product/models/stock_picking.py
@@ -3,36 +3,43 @@
 from odoo import models, fields, api
 from odoo.tools import pdf
 
+import base64
+import io
+
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     def _action_done(self):
-        super(StockPicking, self)._action_done()
-        for picking in self:
-            pdfs = []
+        result = super(StockPicking, self)._action_done()
+        if self.picking_type_id.sequence_code == "OUT":
+            for picking in self:
+                pdfs = []
 
-            attachment = self.env['ir.attachment'].search([('res_id', '=', picking.id)])
-            # pdfs.append(attachment.datas)
+                attachment = self.env['ir.attachment'].search([('res_id', '=', picking.id)])
+                try:
+                    pdfs.append(base64.decodebytes(attachment.datas))
+                except:
+                    pass
 
-            delivery_slip = self.env.ref('stock.action_report_delivery', raise_if_not_found=True)
-            delivery_slip_pdf, _ = delivery_slip._render_qweb_pdf(self.id)
-            pdfs.append(delivery_slip_pdf)
+                delivery_slip = self.env.ref('stock.action_report_delivery', raise_if_not_found=True)
+                delivery_slip_pdf, _ = delivery_slip._render_qweb_pdf(self.id)
+                pdfs.append(delivery_slip_pdf)
 
-            inv = self.env.ref('account.account_invoices', raise_if_not_found=True)
-            inv_pdf, _ = inv._render_qweb_pdf(self.sale_id.invoice_ids.ids)
-            pdfs.append(inv_pdf)
+                inv = self.env.ref('account.account_invoices', raise_if_not_found=True)
+                inv_pdf, _ = inv._render_qweb_pdf(self.sale_id.invoice_ids.ids)
+                pdfs.append(inv_pdf)
 
-            merged_pdf = pdf.merge_pdf(pdfs)
-
-            return {
-                'type': 'ir.actions.report',
-                'report_type': pdf,
-                'data': merged_pdf,
-                'name': self.name + '_combined',
-            }
+                merged_pdf = pdf.merge_pdf(pdfs)
+                
+                picking.message_post(
+                    attachments=[("Delivery_Combined_{}.pdf".format(picking.name), merged_pdf)],
+                    body="Merged document of delivery slip, shipping label, and invoice.",
+                )
+        return result
 
     def custom_button_validate(self):
-        # return self.action_save_then_validate()
+        if not self.picking_type_id.sequence_code == "OUT":
+            return self.button_validate()
         return {
             'name': 'Delivery Warning Wizard',
             'type': 'ir.actions.act_window',
