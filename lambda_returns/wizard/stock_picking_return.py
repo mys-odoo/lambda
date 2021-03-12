25bd6c9ae2e83460a00677766d49a95cfa63b3c9
cic-odoo <cic@odoo.com>
2021-03-12 15:19:17 -0800

    [IMP] changes to the behavior by splitting the return by bill with the associated product.

--- lambda_returns/wizard/stock_picking_return.py
+++ lambda_returns/wizard/stock_picking_return.py
@@ -10,43 +10,41 @@ class ReturnPicking(models.TransientModel):
     _inherit = 'stock.return.picking'
 
     def _create_returns(self):
-
         picking_id, picking_type_id = super(ReturnPicking, self)._create_returns()
-        print("Hello")
-
-        for test in self.product_return_moves:
-            print("checking")
 
         # Get the return picking (Transfer Receipt)
         pick = self.env['stock.picking'].browse(picking_id)
 
         inv_ids = pick.purchase_id.invoice_ids.filtered(lambda i: i.state == 'posted')
 
-        # Only look at the invoices that are related
-        for inv in pick.purchase_id.invoice_ids.filtered(lambda i: i.state == 'posted'):
-            # Create the credit note if inv matches the move POL
-            # If the PO id matches in the stock moves and in the invoice lines of the related Bill(inv)
-            sml = pick.move_line_ids_without_package.filtered(lambda ml: ml.move_id.purchase_line_id in inv.invoice_line_ids.purchase_line_id)
-            if sml:
+        # group the SML into bills that have the sml's product
+        bills = {inv.id: self.env['stock.move.line'] for inv in inv_ids}
+
+        for sml in pick.move_line_ids_without_package:
+            matching_inv = inv_ids.filtered(lambda i: sml.product_id in i.invoice_line_ids.product_id)
+            if matching_inv:
+                bills[matching_inv[0].id] += sml
+
+        for inv_id in bills:
+            # Create a credit note for each bill that has the sml from the return pick
+            if bills[inv_id]:
                 reversal = self.env['account.move.reversal'].create({
-                    'move_ids': [(4, inv.id, 0)],
+                    'move_ids': [(4, inv_id, 0)],
                     'refund_method': 'refund',
                 })
                 res = reversal.reverse_moves()
-                credit_note = self.env['account.move'].search([('reversed_entry_id', '=', inv.id)],
+                credit_note = self.env['account.move'].search([('reversed_entry_id', '=', inv_id)],
                                                               order='create_date desc', limit=1)
-                # TODO: check what this does from EHE's old code from aryse
-                # credit_note.sudo().update_with_picking(self)
-
+                # Update the lines in the credit note to match amount from return, else zero it
+                changed_lines = []
                 for inv_line in credit_note.invoice_line_ids:
-                    print("i'm at this line")
-                    move_line = sml.filtered(lambda ml: ml.product_id == inv_line.product_id)
+                    move_line = bills[inv_id].filtered(lambda ml: ml.product_id == inv_line.product_id)
                     if move_line:
-                        update_qty = move_line.product_uom_qty
+                        changed_lines.append((1, inv_line.id, {'quantity': move_line.product_uom_qty}))
                     else:
-                        update_qty = 0.0
-                    # TODO: v13+ accounting, deal with unbalanced credit issues
-                    inv_line.update({'quantity': update_qty})
-                    self._onchange_invoice_line_ids()
+                        changed_lines.append((2, inv_line.id, 0))
 
+
+                credit_note.write({'invoice_line_ids': changed_lines})
+                credit_note._onchange_invoice_line_ids()
         return picking_id, picking_type_id
